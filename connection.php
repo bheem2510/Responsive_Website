196c77f0e163684668e28f7e02cadc6eb3de7a7a
Bheem Mastkar <111329157+bheem2510@users.noreply.github.com>
2022-11-17 18:18:26 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
-        die("Failed to connect with MySQL: ". mysqli_connect_error());  
+        die(" Failed to connect with MySQL: ". mysqli_connect_error());  
     }  
 ?>  
 authenticat
