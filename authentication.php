dcedc22d4289e89e86c82ee01c477b7994902d93
Bheem Mastkar <111329157+bheem2510@users.noreply.github.com>
2022-11-17 18:16:22 +0530

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -11,7 +11,7 @@
       
         $sql = "select *from login where username = '$username' and password = '$password'";  
         $result = mysqli_query($con, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $row = mysqli_fetch_array($result,  MYSQLI_ASSOC);  
         $count = mysqli_num_rows($result);  
           
         if($count == 1){  
