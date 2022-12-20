d4aefe9abdd3969ed37c7c4ce356fa0f450771d0
Bheem Mastkar <111329157+bheem2510@users.noreply.github.com>
2022-12-20 17:06:31 +0530

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -18,6 +18,7 @@
         if($count == 1){  
             echo "<h1><center> Login successful </center></h1>";  
         }  
+
         else{  
             echo "<h1> Login failed. Invalid username or password.</h1>";  
         }     
