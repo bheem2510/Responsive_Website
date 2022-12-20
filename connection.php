c030fcb720e3591fc57faf5d616cdd3eb2331299
Bheem Mastkar <111329157+bheem2510@users.noreply.github.com>
2022-12-20 17:07:11 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,7 +4,8 @@
     $password = '';  
     $db_name =  "bheem";  
       
-    $con = mysqli_connect($host, $user, $password, $db_name);  
+    $con = mysqli_connect($host, $user, $password, $db_name); 
+
     if(mysqli_connect_errno()) {  
         die(" Failed to connect with MySQL: ". mysqli_connect_error());  
     }  
