596ca9738877b122f81d104b1574a6f67bac1823
Bheem Mastkar <111329157+bheem2510@users.noreply.github.com>
2022-12-20 17:06:43 +0530

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -15,6 +15,7 @@
         $row = mysqli_fetch_array($result,  MYSQLI_ASSOC);  
         $count =  mysqli_num_rows($result);  
           
+
         if($count == 1){  
             echo "<h1><center> Login successful </center></h1>";  
         }  
